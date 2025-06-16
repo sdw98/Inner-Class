fa95fb44c913680ffe1da1650cf8e54f9fba9c09
sdw98 <sdw9851@gmail.com>
2025-06-16 11:03:05 +0900

    first commit

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+class Car {
+    String brand;
+
+    public Car (String brand){
+        this.brand = brand;
+    }
+
+    class Engine {
+        public void start() {
+            System.out.println("The engine of " + brand + " is starting");
+        }
+    }
+
+    public void startEngine() {
+        Engine engine = new Engine();
+        engine.start();
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car car = new Car("빠른 자동차");
+
+        car.startEngine();
+    }
+}
